03d3d2bb2de09eed39c7714840b63e2caf8a2df6
Marieee1227 <126005620+Marieee1227@users.noreply.github.com>
2023-02-21 12:15:43 -0500

    Add files via upload

---
+++ IBM Data Science.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Yiwei Xu**
+# 
+# Data Analyst
+
+# *I am interested in data science because it is fun*
+
+# ### 1+1
+
+# In[2]:
+
+
+print(1+1)
+
+
+# Horizontal Rule
+# 
+# ---
+# 
+# 1. first item 
+# - Unordered
+
+# In[ ]:
+
+
+
+
